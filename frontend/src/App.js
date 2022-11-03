8d7e15ad0741e54633e5a0bbe20398e1aef69dbd
Vitor Pereira <vitorpbemp@gmail.com>
2022-11-03 19:57:14 -0300

    project init

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Drink Free</a>
+      </header>
+      <main>
+        products
+      </main>
+    </div>
+  );
+}
+
+export default App;
